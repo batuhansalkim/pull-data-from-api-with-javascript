e65b58584f2bab7b70a917cf1fe88494a03baac4
Baturpcu <batusalkim11@gmail.com>
2023-02-05 23:28:15 +0300

    last update
    last update

---
+++ pull data from api with javascript/Javascript İle JSON Verilerini Çekme/hex.js
@@ -0,0 +1,24 @@
+
+var data = fetch("veri.json")
+.then(response =>response.json())
+.then(veri=>{
+    console.log(veri);
+    console.log(veri.kullanicilar[1]);
+    console.log(veri.yetki[0]);
+    var verimiz = JSON.stringify(veri);
+    console.log(verimiz);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
