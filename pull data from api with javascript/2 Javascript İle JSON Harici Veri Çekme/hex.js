f888f4e7523013df98e50735abbfd6328fe34b15
Baturpcu <batusalkim11@gmail.com>
2023-02-06 00:36:31 +0300

    last update

--- pull data from api with javascript/2 Javascript İle JSON Harici Veri Çekme/hex.js
+++ pull data from api with javascript/2 Javascript İle JSON Harici Veri Çekme/hex.js
@@ -1,6 +1,25 @@
-let data = fetch("https://jsonplaceholder.typicode.com/posts")
+// var listele = document.getElementById("listele");
+// let data = fetch("https://jsonplaceholder.typicode.com/photos")
+// .then(response=>response.json())
+// .then(veri=>{
+//     veri.forEach(element => {
+//       if(element.id<=9){
+//           listele.innerHTML += `<li>${element.body}</li>` 
+//       }
+//     });  
+// });
+
+var listele = document.getElementById("album");
+
+let data = fetch("https://jsonplaceholder.typicode.com/photos")
 .then(response=>response.json())
-.then(veri=>console.log(veri));
+.then(veri =>{
+    veri.forEach(element=>{
+        if(element.id<=30){
+            listele.innerHTML += `<img src= "${element.url}" />`
+        }
+    })
+})
 
 
 
