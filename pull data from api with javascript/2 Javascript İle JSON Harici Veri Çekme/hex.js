66f2bed2e8885d77d1d740cad11d13750be75fc1
Baturpcu <batusalkim11@gmail.com>
2023-02-06 02:32:55 +0300

    update

--- pull data from api with javascript/2 Javascript İle JSON Harici Veri Çekme/hex.js
+++ pull data from api with javascript/2 Javascript İle JSON Harici Veri Çekme/hex.js
@@ -1,14 +1,3 @@
-// var listele = document.getElementById("listele");
-// let data = fetch("https://jsonplaceholder.typicode.com/photos")
-// .then(response=>response.json())
-// .then(veri=>{
-//     veri.forEach(element => {
-//       if(element.id<=9){
-//           listele.innerHTML += `<li>${element.body}</li>` 
-//       }
-//     });  
-// });
-
 var listele = document.getElementById("album");
 
 let data = fetch("https://jsonplaceholder.typicode.com/photos")
---
+++ pull data from api with javascript/3 JSON Nedir ve JSON Metodları/hex.js
@@ -0,0 +1,38 @@
+let data = {
+    kisiler:[
+        {name:"Batuhan", surname:"salkım",age:"21"},
+        {name:"Tunahan", surname:"salkım",age:"16"},
+        {name:"Necla", surname:"salkım",age:"46"},
+        {name:"Mustafa", surname:"salkım",age:"47"},
+    ],
+    islem:"Get"
+}
+
+console.log(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
