42284345391392ed97a8714b0d6814b30d5cb0fd
Baturpcu <batusalkim11@gmail.com>
2023-02-07 04:26:33 +0300

    update

--- pull data from api with javascript/5 Canlı Döviz Kuru Nasıl Çekilir/hex.js
+++ pull data from api with javascript/5 Canlı Döviz Kuru Nasıl Çekilir/hex.js
@@ -1,9 +1,32 @@
 const api = 'https://api.exchangeratesapi.io/latest?base=TRY'
 
 
+function getData(api){
+    fetch(api).then(res=>res.json()).then(data=>{
+        console.log(data.rates);
+
+        const TRY = data.rates.TRY;
+        const USD = TRY / data.rates.USD;
+        const EUR = TRY / data.rates.EUR;
+        const GBP = TRY / data.rates.GBP;
 
 
+        setData(USD,EUR,GBP);
 
+    }).catch((err) => console.warn(err)); 
+}
+
+
+function setData(USD,EUR,GBP){
+    const dollar = document.getElementById("dollar");
+    const euro = document.getElementById("euro");
+    const sterlin = document.getElementById("sterlin");
+
+    dollar.textContent = USD.toString().slice(0, 4);
+    euro.textContent = EUR.toString().slice(0, 4);
+    sterlin.textContent = GBP.toString().slice(0, 4);
+}
+getData(api); 
 
 
 
