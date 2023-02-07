50a63f8466e01abaeed77de69c18bbb8ef267270
Baturpcu <batusalkim11@gmail.com>
2023-02-07 04:42:42 +0300

    update

--- pull data from api with javascript/5 Canlı Döviz Kuru Nasıl Çekilir/hex.js
+++ pull data from api with javascript/5 Canlı Döviz Kuru Nasıl Çekilir/hex.js
@@ -1,32 +1,35 @@
 const api = 'https://api.exchangeratesapi.io/latest?base=TRY'
 
-
 function getData(api){
     fetch(api).then(res=>res.json()).then(data=>{
         console.log(data.rates);
 
-        const TRY = data.rates.TRY;
+        const TRY =data.rates.TRY;
         const USD = TRY / data.rates.USD;
         const EUR = TRY / data.rates.EUR;
         const GBP = TRY / data.rates.GBP;
 
-
-        setData(USD,EUR,GBP);
-
-    }).catch((err) => console.warn(err)); 
+        setData(TRY,USD,GBP);
+    }).catch((err) =>console.warn(err));
 }
 
+getData(api);
 
-function setData(USD,EUR,GBP){
+function setData(USD,EUR,TRY){
     const dollar = document.getElementById("dollar");
     const euro = document.getElementById("euro");
     const sterlin = document.getElementById("sterlin");
 
-    dollar.textContent = USD.toString().slice(0, 4);
-    euro.textContent = EUR.toString().slice(0, 4);
-    sterlin.textContent = GBP.toString().slice(0, 4);
+    dollar.textContent = USD.toString.slice(0, 4);
+    euro.textContent = EUR.toString.slice(0, 4);
+    sterlin.textContent = GBP.toString.slice(0, 4);
 }
-getData(api); 
+getData(api);
+setInterval(()=>{
+    getData(api);
+},30000);
+getData(api);
+
 
 
 
