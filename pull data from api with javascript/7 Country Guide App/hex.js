0f3b5e0983233e4f0a32e812e56bda95b69220e1
Baturpcu <batusalkim11@gmail.com>
2023-02-11 23:16:16 +0300

    update

--- pull data from api with javascript/7 Country Guide App/hex.js
+++ pull data from api with javascript/7 Country Guide App/hex.js
@@ -1,60 +1,32 @@
 let searchBtn = document.getElementById("search-btn");
-
 let countryInp = document.getElementById("country-inp");
-
-let result = document.getElementsByClassName("result");
-
-searchBtn.addEventListener("click",()=>{
-    let countryName = "India";
-    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+searchBtn.addEventListener("click", () => {
+    let countryName = "turkey";
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL);
     fetch(finalURL)
-    .then((response)=> response.json())
-    .then((data)=> {
-        console.log(data[0]);
-        console.log(data[0].capital);
-        console.log(data[0].continents[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(Object.keys(data[0].currencies)[0]);
+        .then((response) => response.json())
+        .then((data) => {
+            //   console.log(data[0]);
+            //   console.log(data[0].capital[0]);
+            //   console.log(data[0].flags.svg);
+            //   console.log(data[0].name.common);
+            //   console.log(data[0].continents[0]);
+            //   console.log(Object.keys(data[0].currencies)[0]);
+            //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+            //   console.log(
+            //     Object.values(data[0].languages).toString().split(",").join(", ")
+            //   );
+            result.innerHTML = `
+                <img src="${data[0].flags.svg}" class="flag-img">
+                <h2>${data[0].name.common}</h2>
+                <div class="wrapper>
+                    <div class="data-wrapper">
+                        <h4>Capital : </h4>
+                        <span>${data[0].capital[0]}</span>
+                    </div>
+                </div>
+            `;
+        })
         
-        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-
-        
-        console.log(
-            Object.values(data[0].languages).toString().split(",").join(",")
-        );
-        result.innerHTML = `
-            <img src="${data[0].flags.svg}" class="flag-img">
-        `;
-    })
-    
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
