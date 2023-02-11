f852a42da0b7821a40d44229b06830085c42a010
Baturpcu <batusalkim11@gmail.com>
2023-02-11 22:00:01 +0300

    update

--- pull data from api with javascript/7 Country Guide App/hex.js
+++ pull data from api with javascript/7 Country Guide App/hex.js
@@ -3,7 +3,7 @@ let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
  
 searchBtn.addEventListener("click",()=>{
-    let countryName = "turkey";
+    let countryName = "India";
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
     console.log(finalURL);
     fetch(finalURL)
@@ -14,6 +14,12 @@ searchBtn.addEventListener("click",()=>{
         console.log(data[0].continents[0]);
         console.log(data[0].flags.svg);
         console.log(data[0].name.common);
+        console.log(Object.keys(data[0].currencies)[0]);
+        
+        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+
+        console.log(data[0].currencies[Object.keys(data[0].currencies)].symbol);
+        console.log(Object.values(data[0].languages).toString().split(",").join(", "));
     })
 })
 
